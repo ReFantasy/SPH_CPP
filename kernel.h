84c705824a5a38363ec87157b0ac546f7c0e9dba
ReFantasy <refantasy@ReFantasydeMacBook-Pro>
2022-03-25 12:31:06 +0800

    .

--- kernel.h
+++ kernel.h
@@ -4,8 +4,11 @@
 
 #ifndef SPH_KERNEL_H
 #define SPH_KERNEL_H
+
 #include "glm/glm.hpp"
 
 float cubic_kernel(glm::vec2 r, float support_radius);
+
 glm::vec2 cubic_kernel_derivative(glm::vec2 r, float support_radius);
+
 #endif //SPH_KERNEL_H
